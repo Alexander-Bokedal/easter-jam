e1aec8ee8ea04487a5db3b180badc945b15e075c
Alexander-Bokedal <alexander.bokedal@gmail.com>
2025-04-04 15:19:16 +0200

    Added submit button and logic for confirming

--- src/components/formComponents/SubmitButton.tsx
+++ src/components/formComponents/SubmitButton.tsx
@@ -1,19 +1,39 @@
 
 'use client'
-import { ButtonHTMLAttributes } from 'react'
+import { ButtonHTMLAttributes, useState } from 'react'
 
 interface SubmitButtonProps extends ButtonHTMLAttributes<HTMLButtonElement> {
-	label?: string
 }
+type SubmitStep = 'idle' | 'code' | 'confirm' | 'submit'
 
-export const SubmitButton = ({ label = "Submit", ...props }: SubmitButtonProps) => {
+export const SubmitButton = ({ }: SubmitButtonProps) => {
+	const [submitStep, setSubmitStep] = useState<SubmitStep>('idle')
+	const Log = () => {
+		console.log('This button was clicked')
+	}
+	const HandleIdleClick = () => {
+		setSubmitStep('submit')
+	}
 	return (
-		<button
-			type="submit"
-			className="mt-6 w-full bg-blue-600 text-white font-medium py-2 px-4 rounded-2xl shadow hover:bg-blue-700 transition"
-			{...props}
-		>
-			{label}
-		</button>
+		<>
+
+			{submitStep === 'idle' ? (
+				<button
+					onClick={HandleIdleClick}
+					className="mt-6 w-full bg-blue-600 text-white font-medium py-2 px-4 rounded-2xl shadow hover:bg-blue-700 transition"
+				>
+					Enter Code
+				</button>
+			) : null}
+			{submitStep === 'submit' ? (
+				<button
+					onClick={Log}
+					type="submit"
+					className="mt-6 w-full bg-blue-600 text-white font-medium py-2 px-4 rounded-2xl shadow hover:bg-blue-700 transition"
+				>
+					Save Egg!
+				</button>
+			) : null}
+		</>
 	)
 }
